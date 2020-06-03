328d2c65fa37aa109b468361cb4ac9c20a864fdd
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2020-06-04 01:06:14 +0200

    Output warnings as GH Checks annotations

--- twine-upload.sh
+++ twine-upload.sh
@@ -7,8 +7,8 @@ if [[
     ! "$INPUT_PASSWORD" =~ ^pypi-
   ]]
 then
-    >&2 echo \
-        [WARNING]: \
+    echo \
+        ::warning file='# >>' PyPA publish to PyPI GHA'%3A' '<< ':: \
         It looks like you are trying to use an API token to \
         authenticate in the package index and your token value does \
         not start with '"pypi-"' as it typically should. This may \
@@ -21,11 +21,12 @@ if [[
     "`ls -l ${INPUT_PACKAGES_DIR%%/}/*.tar.gz ${INPUT_PACKAGES_DIR%%/}/*.whl`" == "total 0"
   ]]
 then
-    >&2 echo \
-        [WARNING]: \
+    echo \
+        ::warning file='# >>' PyPA publish to PyPI GHA'%3A' '<< ':: \
         It looks like there are no Python distribution packages to \
-        publish in the directory '"${INPUT_PACKAGES_DIR%%/}/"'. Please verify that they \
-        are in place should you face this problem.
+        publish in the directory '"${INPUT_PACKAGES_DIR%%/}/"'. \
+        Please verify that they are in place should you face this \
+        problem.
 fi
 
 if [[ ${INPUT_VERIFY_METADATA,,} != "false" ]] ; then
