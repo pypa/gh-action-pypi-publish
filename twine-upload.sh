e5cc29fe08bb1ec972cc3b5b597e9dde2ba8c2b4
meowmeowcat <68463158+meowmeowmeowcat@users.noreply.github.com>
2022-01-08 00:24:27 +0800

    Show hash values of files uploaded

--- twine-upload.sh
+++ twine-upload.sh
@@ -48,3 +48,29 @@ TWINE_USERNAME="$INPUT_USER" \
 TWINE_PASSWORD="$INPUT_PASSWORD" \
 TWINE_REPOSITORY_URL="$INPUT_REPOSITORY_URL" \
   exec twine upload ${TWINE_EXTRA_ARGS} ${INPUT_PACKAGES_DIR%%/}/*
+
+if [[ ${INPUT_PRINT_HASH,,} != "false" ]] ; then
+    cat > ./print_hash.py << EOF
+import os
+import hashlib
+sha256 = hashlib.sha256()
+md5 = hashlib.md5()
+blake2_256 = hashlib.blake2b(digest_size=256 // 8)
+file_list = os.listdir(os.path.abspath("${INPUT_PACKAGES_DIR%%/}"))
+for i in file_list:
+    print(i)
+    print("")
+    file = open(os.path.abspath(os.path.join("${INPUT_PACKAGES_DIR%%/}", i)), "rb")
+    content = file.read()
+    file.close()
+    sha256.update(content)
+    md5.update(content)
+    blake2_256.update(content)
+    print(f"SHA256: {sha256.hexdigest()}")
+    print(f"MD5: {md5.hexdigest()}")
+    print(f"BLAKE2-256: {blake2_256.hexdigest()}")
+    print("")
+EOF
+    python ./print_hash.py
+    rm ./print_hash.py
+fi
