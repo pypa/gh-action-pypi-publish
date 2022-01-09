717ba43cfbb0387f6ce311b169a825772f54d295
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2022-01-09 12:53:19 +0100

    Trim the trailing whitespaces in `print-hash.py`

--- print-hash.py
+++ print-hash.py
@@ -10,12 +10,12 @@ for file_object in packages_dir.iterdir():
     sha256 = hashlib.sha256()
     md5 = hashlib.md5()
     blake2_256 = hashlib.blake2b(digest_size=256 // 8)
-    
+
     print(file_object)
     print("")
 
     content = file_object.read_bytes()
-    
+
     sha256.update(content)
     md5.update(content)
     blake2_256.update(content)
