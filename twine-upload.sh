6013b721b829c612d93d907c284c82505208b7c2
shenxianpeng <xianpeng.shen@gmail.com>
2024-04-21 23:52:03 +0800

    Disable the progress bar when exec twine upload

--- twine-upload.sh
+++ twine-upload.sh
@@ -39,6 +39,7 @@ INPUT_PACKAGES_DIR="$(get-normalized-input 'packages-dir')"
 INPUT_VERIFY_METADATA="$(get-normalized-input 'verify-metadata')"
 INPUT_SKIP_EXISTING="$(get-normalized-input 'skip-existing')"
 INPUT_PRINT_HASH="$(get-normalized-input 'print-hash')"
+INPUT_DISABLE_PROGRESS_BAR="$(get-normalized-input 'disable-progress-bar')"
 
 PASSWORD_DEPRECATION_NUDGE="::error title=Password-based uploads deprecated::\
 Starting in 2024, PyPI will require all users to enable Two-Factor \
@@ -132,6 +133,8 @@ fi
 if [[ ${INPUT_PRINT_HASH,,} != "false" || ${INPUT_VERBOSE,,} != "false" ]] ; then
     python /app/print-hash.py ${INPUT_PACKAGES_DIR%%/}
 fi
+
+TWINE_EXTRA_ARGS="--disable-progress-bar $TWINE_EXTRA_ARGS"
 
 TWINE_USERNAME="$INPUT_USER" \
 TWINE_PASSWORD="$INPUT_PASSWORD" \
