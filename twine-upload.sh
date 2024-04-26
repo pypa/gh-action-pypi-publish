1fa98bf3320532b4cc5a305281337103b0d77123
William Woodruff <william@trailofbits.com>
2024-04-26 12:44:25 -0400

    twine-upload: fix tense on password nudge
    Also makes password use a hard error on PyPI.
    
    Signed-off-by: William Woodruff <william@trailofbits.com>

--- twine-upload.sh
+++ twine-upload.sh
@@ -40,9 +40,9 @@ INPUT_VERIFY_METADATA="$(get-normalized-input 'verify-metadata')"
 INPUT_SKIP_EXISTING="$(get-normalized-input 'skip-existing')"
 INPUT_PRINT_HASH="$(get-normalized-input 'print-hash')"
 
-PASSWORD_DEPRECATION_NUDGE="::error title=Password-based uploads deprecated::\
-Starting in 2024, PyPI will require all users to enable Two-Factor \
-Authentication. This will consequently require all users to switch \
+PASSWORD_DEPRECATION_NUDGE="::error title=Password-based uploads disabled::\
+As of 2024, PyPI requires all users to enable Two-Factor \
+Authentication. This consequentlys require all users to switch \
 to either Trusted Publishers (preferred) or API tokens for package \
 uploads. Read more: \
 https://blog.pypi.org/posts/2023-05-25-securing-pypi-with-2fa/"
@@ -74,6 +74,7 @@ else
     if [[ "${INPUT_REPOSITORY_URL}" =~ pypi\.org ]]; then
         echo "${PASSWORD_DEPRECATION_NUDGE}"
         echo "${TRUSTED_PUBLISHING_NUDGE}"
+        exit 1
     fi
 fi
 
