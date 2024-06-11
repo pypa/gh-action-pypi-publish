1571a0dc4b7925b0b8317b71a031dcb1cf2edc69
William Woodruff <william@trailofbits.com>
2024-06-11 17:35:16 -0400

    bump pypi_attestation_models, update usage
    Signed-off-by: William Woodruff <william@trailofbits.com>

--- attestations.py
+++ attestations.py
@@ -4,16 +4,16 @@ import sys
 from pathlib import Path
 from typing import NoReturn
 
-from pypi_attestation_models import AttestationPayload
+from pypi_attestation_models import Attestation
 from sigstore.oidc import IdentityError, IdentityToken, detect_credential
 from sigstore.sign import Signer, SigningContext
 
 # Be very verbose.
-sigstore_logger = logging.getLogger('sigstore')
+sigstore_logger = logging.getLogger("sigstore")
 sigstore_logger.setLevel(logging.DEBUG)
 sigstore_logger.addHandler(logging.StreamHandler())
 
-_GITHUB_STEP_SUMMARY = Path(os.getenv('GITHUB_STEP_SUMMARY'))
+_GITHUB_STEP_SUMMARY = Path(os.getenv("GITHUB_STEP_SUMMARY"))
 
 # The top-level error message that gets rendered.
 # This message wraps one of the other templates/messages defined below.
@@ -44,34 +44,33 @@ Learn more at https://docs.github.com/en/actions/deployment/security-hardening-y
 
 
 def die(msg: str) -> NoReturn:
-    with _GITHUB_STEP_SUMMARY.open('a', encoding='utf-8') as io:
+    with _GITHUB_STEP_SUMMARY.open("a", encoding="utf-8") as io:
         print(_ERROR_SUMMARY_MESSAGE.format(message=msg), file=io)
 
     # HACK: GitHub Actions' annotations don't work across multiple lines naively;
     # translating `\n` into `%0A` (i.e., HTML percent-encoding) is known to work.
     # See: https://github.com/actions/toolkit/issues/193
-    msg = msg.replace('\n', '%0A')
-    print(f'::error::Attestation generation failure: {msg}', file=sys.stderr)
+    msg = msg.replace("\n", "%0A")
+    print(f"::error::Attestation generation failure: {msg}", file=sys.stderr)
     sys.exit(1)
 
 
 def debug(msg: str):
-    print(f'::debug::{msg}', file=sys.stderr)
+    print(f"::debug::{msg}", file=sys.stderr)
 
 
 # pylint: disable=redefined-outer-name
 def attest_dist(dist: Path, signer: Signer) -> None:
     # We are the publishing step, so there should be no pre-existing publish
     # attestation. The presence of one indicates user confusion.
-    attestation_path = Path(f'{dist}.publish.attestation')
+    attestation_path = Path(f"{dist}.publish.attestation")
     if attestation_path.is_file():
-        die(f'{dist} already has a publish attestation: {attestation_path}')
+        die(f"{dist} already has a publish attestation: {attestation_path}")
 
-    payload = AttestationPayload.from_dist(dist)
-    attestation = payload.sign(signer)
+    attestation = Attestation.sign(signer, dist)
 
-    attestation_path.write_text(attestation.model_dump_json(), encoding='utf-8')
-    debug(f'saved publish attestation: {dist=} {attestation_path=}')
+    attestation_path.write_text(attestation.model_dump_json(), encoding="utf-8")
+    debug(f"saved publish attestation: {dist=} {attestation_path=}")
 
 
 def get_identity_token() -> IdentityToken:
@@ -94,8 +93,8 @@ except IdentityError as identity_error:
     die(cause)
 
 # Collect all sdists and wheels.
-dists = [sdist.absolute() for sdist in packages_dir.glob('*.tar.gz')]
-dists.extend(whl.absolute() for whl in packages_dir.glob('*.whl'))
+dists = [sdist.absolute() for sdist in packages_dir.glob("*.tar.gz")]
+dists.extend(whl.absolute() for whl in packages_dir.glob("*.whl"))
 
 with SigningContext.production().signer(identity, cache=True) as signer:
     for dist in dists:
@@ -103,6 +102,6 @@ with SigningContext.production().signer(identity, cache=True) as signer:
         # download-artifact will create a subdirectory with the same name
         # as the artifact being downloaded, e.g. `dist/foo.whl/foo.whl`.
         if not dist.is_file():
-            die(f'Path looks like a distribution but is not a file: {dist}')
+            die(f"Path looks like a distribution but is not a file: {dist}")
 
         attest_dist(dist, signer)
